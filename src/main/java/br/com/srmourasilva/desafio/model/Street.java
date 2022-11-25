80f9485c3e74cc367d1cad7b89dbd0f0b8fb5923
Paulo Mateus <mateus.moura@hotmail.com>
2022-11-25 09:19:29 -0300

    fix(user): missing getters/setters

--- src/main/java/br/com/srmourasilva/desafio/model/Street.java
+++ src/main/java/br/com/srmourasilva/desafio/model/Street.java
@@ -15,4 +15,28 @@ public class Street {
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Optional<Integer> getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = Optional.ofNullable(number);
+    }
+
+    public Optional<String> getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = Optional.ofNullable(complement);
+    }
 }
--- src/main/java/br/com/srmourasilva/desafio/model/User.java
+++ src/main/java/br/com/srmourasilva/desafio/model/User.java
@@ -71,4 +71,12 @@ public class User {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
 }
